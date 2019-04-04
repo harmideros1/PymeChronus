590ab063d096c26fb08f4702b1bc68825225c80b
Harold Mideros <harmideros@gmail.com>
2019-04-04 16:55:15 -0500

    Firebase Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyClp9PRzW8m0NXq7ILja3_16tG9Uw2IrWo",
+    authDomain: "pymechronus.firebaseapp.com",
+    databaseURL: "https://pymechronus.firebaseio.com",
+    projectId: "pymechronus",
+    storageBucket: "pymechronus.appspot.com",
+    messagingSenderId: "1035851945919"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyClp9PRzW8m0NXq7ILja3_16tG9Uw2IrWo",
+    authDomain: "pymechronus.firebaseapp.com",
+    databaseURL: "https://pymechronus.firebaseio.com",
+    projectId: "pymechronus",
+    storageBucket: "pymechronus.appspot.com",
+    messagingSenderId: "1035851945919"
+  }
 };
 
 /*
